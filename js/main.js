aab80ef7641aa020d176cec06aedcb4f9f50311e
RijkvanZanten <rijkvanzanten@me.com>
2016-10-02 21:24:20 +0200

    add gradient background

--- js/main.js
+++ js/main.js
@@ -46,6 +46,37 @@ const yPosition = d3.random.normal(height / 2, height / 9);
 const hexbin = d3.hexbin()
   .radius(height / 9);
 
+const gradient = svg.append('svg:defs')
+  .append('svg:linearGradient')
+    .attr('id', 'gradient')
+    .attr('x1', '0%')
+    .attr('y1', '0%')
+    .attr('x2', '100%')
+    .attr('y2', '0%')
+    .attr('spreadMethod', 'pad');
+
+gradient.append('svg:stop')
+  .attr('offset', '0%')
+  .attr('stop-color', 'yellow')
+  .attr('stop-opacity', 0.6);
+
+gradient.append('svg:stop')
+  .attr('offset', '100%')
+  .attr('stop-color', 'red')
+  .attr('stop-opacity', 0.6);
+
+svg.append('svg:rect')
+  .attr('width', width)
+  .attr('height', height)
+  .style('fill', 'url(#gradient)');
+
+gradient.select('stop')
+  .transition()
+  .duration(10000)
+  .attr('stop-color', 'blue');
+
+svg.append('g')
+  .attr('id', 'keystrokes');
 
 /**
  * Gets volume from current keyboard audio and loops the render function
@@ -70,7 +101,7 @@ function renderLoop() {
 function render() {
   const data = hexbin([[xPosition(), yPosition()]]);
 
-  const hexagons = svg.selectAll('.hexagon').data(data, (d) => d);
+  const hexagons = svg.select('#keystrokes').selectAll('.hexagon').data(data, (d) => d);
 
   hexagons
     .exit()
@@ -83,14 +114,14 @@ function render() {
     .remove();
 
   hexagons.enter(data)
-    .append('path')
-    .attr('transform', (d) => `translate(${d.x}, ${d.y}) scale(0.9)`)
-    .transition()
-    .duration(250)
-      .attr('transform', (d) => `translate(${d.x}, ${d.y})`)
+      .append('path')
+      .attr('transform', (d) => `translate(${d.x}, ${d.y}) scale(0.9)`)
       .attr('class', 'hexagon')
       .attr('d', (d) => hexbin.hexagon())
       .style('fill', 'white')
+    .transition()
+      .duration(250)
+        .attr('transform', (d) => `translate(${d.x}, ${d.y})`)
     .transition()
     .delay(300)
       .attr('transform', (d) => `translate(${d.x}, ${d.y}) scale(0.5)`)
