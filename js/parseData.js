8098d68afcb10bd0444dd13662506292ec2a5e3a
RijkvanZanten <rijkvanzanten@me.com>
2016-10-13 13:03:17 +0200

    create parsing function for window csv

--- js/parseData.js
+++ js/parseData.js
@@ -1,4 +1,6 @@
 /* global d3 */
+
+// Keystrokes
 function parseData(json) {
   const keystrokes = json.map((d) => {
     return {
@@ -27,3 +29,34 @@ function parseData(json) {
 
   return keyStrokesByHourParsed;
 }
+
+// Window
+d3.csv('./active_window.csv', (data) => {
+  data = data.map((d) => {
+    return {
+      date: new Date(Number(d.date) * 1000),
+      window: d.window
+    };
+  });
+
+  windowByHour = d3.nest()
+  .key((d) => `${d.date.getDate()}-${d.date.getMonth()}`)
+  .key((d) => `${d.date.getHours()}:00`)
+  .rollup((v) => {
+    return {
+      date: new Date(2016, v[0].date.getMonth(), v[0].date.getDate(), v[0].date.getHours(), 0),
+      windows: v.map((obj) => obj.window).reduce((acc, curr) => {
+        if (typeof acc[curr] == 'undefined') {
+          acc[curr] = 1;
+        } else {
+          acc[curr] += 1;
+        }
+
+        return acc;
+      }, {})
+    };
+  })
+  .entries(data);
+
+  console.log(windowByHour);
+});
